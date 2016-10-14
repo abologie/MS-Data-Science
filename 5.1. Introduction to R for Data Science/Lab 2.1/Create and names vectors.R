19fd42bd7d14c8c57877fdc24d58bb2fa414baba
iordan93 <iordan93@gmail.com>
2016-10-14 21:24:26 +0300

    Added lab 2.1

---
+++ 5.1. Introduction to R for Data Science/Lab 2.1/Create and names vectors.R
@@ -0,0 +1,39 @@
+# Create a vector (1)
+numeric_vector <- c(1, 10, 49)
+character_vector <- c("a", "b", "c")
+
+# Create boolean_vector
+boolean_vector <- c(TRUE, FALSE, TRUE)
+
+# Create a vector (2)
+# Poker winnings from Monday to Friday
+poker_vector <- c(140, -50, 20, -120, 240)
+
+# Roulette winnings from Monday to Friday: roulette_vector
+roulette_vector <- c(-24, -50, 100, -350, 10)
+
+# Naming a vector (1)
+# Poker winnings from Monday to Friday
+poker_vector <- c(140, -50, 20, -120, 240)
+
+# Roulette winnings from Monday to Friday
+roulette_vector <- c(-24, -50, 100, -350, 10)
+
+# Add names to both poker_vector and roulette_vector
+days <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+names(poker_vector) <- days
+names(roulette_vector) <- days
+
+# Naming a vector (2)
+# Poker winnings from Monday to Friday
+poker_vector <- c(140, -50, 20, -120, 240)
+
+# Roulette winnings from Monday to Friday
+roulette_vector <- c(-24, -50, 100, -350, 10)
+
+# Create the variable days_vector
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+ 
+# Assign the names of the day to roulette_vector and poker_vector
+names(poker_vector) <- days_vector
+names(roulette_vector) <- days_vector
