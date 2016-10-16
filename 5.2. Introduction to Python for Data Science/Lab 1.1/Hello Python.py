b5b4da1ffc145bba79f6862090a8d583a2844c08
iordan93 <iordan93@gmail.com>
2016-10-16 03:06:51 +0300

    Added the labs for the first module of the course Introduction to Python for Data Science

---
+++ 5.2. Introduction to Python for Data Science/Lab 1.1/Hello Python.py
@@ -0,0 +1,28 @@
+# The Python Interface
+print(5 / 8)
+print(7 + 10)
+
+# Any comments?
+# Just testing division
+print(5 / 8)
+
+# Addition works too
+print(7 + 10)
+
+# Python as a calculator
+# Addition and subtraction
+print(5 + 5)
+print(5 - 5)
+
+# Multiplication and division
+print(3 * 5)
+print(10 / 2)
+
+# Exponentiation
+print(4 ** 2)
+
+# Modulo
+print(18 % 7)
+
+# How much is your $100 worth after 7 years?
+print(100 * 1.1 ** 7)
